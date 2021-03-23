347638a37b6198a54b1fad4365c037b2a27fd455
Valtteri Laine <valtteri.laine@vr.fi>
2021-03-23 16:28:48 +0200

    Adds comment

--- App.js
+++ App.js
@@ -7,6 +7,14 @@ import {
   AccessibilityInfo,
 } from 'react-native';
 
+// Problem
+
+/**
+ * on android pressing the button will result in "button pressed" being read out
+ * On iOS presing the button results in the new text content of the button being read out, and announceForAccessibility is ignored.
+
+ * Optimally we would like it to read "Button pressed", and then announce new text content of button
+ */
 const App = () => {
   const initialText = 'initial text';
   const [btnText, setBtnText] = useState(initialText);
@@ -16,7 +24,6 @@ const App = () => {
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
         <TouchableOpacity
-          accessibilityLiveRegion="none"
           onPress={() => {
             if (btnText === initialText) {
               setBtnText('Other text');
